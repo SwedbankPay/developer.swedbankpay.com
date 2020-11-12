061a28f865525ed77b4317346bd13446dbd2511c
deploy.sh <asbjorn@ulsberg.no>
2020-11-12 07:53:43 +0100

    publish: Merge pull request #756 from SwedbankPay/release/2.0.0
    generated from commit adf2546cee92efdbcb97024b9d81214d51204f62

--- assets/js/swedbank-pay-design-guide-theme.js
+++ assets/js/swedbank-pay-design-guide-theme.js
@@ -20,54 +20,111 @@
 
 // Initialize sidebar navigation scroll activation
 (function () {
-    var headings = document.querySelectorAll("h2");
-    var tocLinks = document.querySelectorAll("nav.doc-toc ul li a");
+    var headings = document.querySelectorAll(".doc-container h2");
+    const activeSubgroup = document.querySelector("nav.sidebar-nav .nav-subgroup.active");
+    var tocLinks = [];
 
-    var getPosition = function (el) {
-        if (el) {
-            var bodyRect = document.body.getBoundingClientRect();
-            var elemRect = el.getBoundingClientRect();
-
-            return elemRect.top - bodyRect.top;
+    if (activeSubgroup) {
+        if (activeSubgroup.querySelector(".nav-subgroup.active")) {
+            tocLinks = activeSubgroup.querySelector(".nav-subgroup.active").querySelectorAll(".nav-leaf");
+        } else {
+            tocLinks = activeSubgroup.querySelectorAll(".nav-leaf");
         }
+        
+    } else {
+        tocLinks = document.querySelectorAll("nav.sidebar-nav .nav-group.active .nav-leaf");
+    }
 
+    var getPosition = function (parent, el) {
+        if (el) {
+            var parentRect = parent.getBoundingClientRect();
+            var elemRect = el.getBoundingClientRect();
+            
+            return elemRect.top - parentRect.top;
+        }
+        
         return null;
     };
 
-    window.addEventListener("scroll", function () {
-        var buffer = document.body.clientHeight * 0.1;
-        var currentPos = window.pageYOffset + buffer;
+    const _handleLeafScrollListener = function () {
+        if (tocLinks.length > 0) {
+            const activeLeaf = document.querySelector("nav.sidebar-nav .nav-leaf.active");
+            const buffer = document.body.clientHeight * 0.1;
+            const currentPos = window.pageYOffset + buffer;
 
-        // TODO: Probably a stupid way to compute "how far left can we scroll until
-        //       we reach the bottom of the page", but it seems to work.
-        var scrollDistanceFromBottom = document.documentElement.scrollHeight
-            - document.documentElement.scrollTop
-            - document.body.clientHeight
-            - buffer;
+            // TODO: Probably a stupid way to compute "how far left can we scroll until
+            //       we reach the bottom of the page", but it seems to work.
+            const scrollDistanceFromBottom = document.documentElement.scrollHeight
+                - document.documentElement.scrollTop
+                - document.body.clientHeight
+                - buffer;
+                
+                
+            activeLeaf && !activeLeaf.classList.contains("nav-subgroup-leaf") && activeLeaf.classList.remove("active");
+                
+                
+            if (scrollDistanceFromBottom > 0) {
+                const scrollNumber = [...headings].filter((heading) => getPosition(document.body, heading) <= currentPos).length - 1;
 
-        if (scrollDistanceFromBottom <= 0) {
-            for (var link of tocLinks) {
-                link.parentElement.classList.remove("active");
+                scrollNumber >= 0 && tocLinks[parseInt(scrollNumber, 10)].classList.add("active");
+            } else {
+                tocLinks[tocLinks.length - 1].classList.add("active");
             }
 
-            tocLinks[tocLinks.length - 1].parentElement.classList.add("active");
-            return;
         }
+    };
 
-        for (var i = 0; i < headings.length; i++) {
-            var heading = headings[i];
-            var headingPos = getPosition(heading);
-            var nextHeadingPos = getPosition(headings[i + 1]);
+    _handleLeafScrollListener();
+    
+    window.addEventListener("scroll", _handleLeafScrollListener);
 
-            if (currentPos > headingPos && currentPos < nextHeadingPos) {
-                for (var link of tocLinks) {
-                    link.parentElement.classList.remove("active");
-                }
+    // Makes sidebar scroll so that the active element is in view
+    const pathHash = window.location.pathname + window.location.hash;
+    const sidebarNav = document.querySelector("nav.sidebar-nav");
+    var activeLeaf = document.querySelector("nav.sidebar-nav .nav-leaf.active");
 
-                tocLinks[i].parentElement.classList.add("active");
-                return;
-            }
+    if (!activeLeaf) {
+        activeLeaf = [...document.querySelectorAll(`nav.sidebar-nav .nav-leaf`)]
+            .filter((navLeaf) => navLeaf.querySelector(`a[href="${pathHash}"]`))[0];
+    }
+
+    if (activeLeaf) {
+        sidebarNav.scrollTop = activeLeaf.offsetTop + activeLeaf.clientHeight - sidebarNav.clientHeight / 2;
+    };
+    
+})();
+
+// Simple sidebar functionality while dg.js is being loaded
+
+function _handleSimpleSidebar (e) {
+    const target = e.target.parentElement.parentElement;
+
+    if (target.tagName === "LI") {
+        if (target.classList.contains("active")) {
+            target.classList.remove("active");
+        } else {
+            const sidebar = document.querySelector(".sidebar");
+            const closeElement = sidebar.querySelector(`.${target.classList[0]}.active`);
+
+            closeElement && closeElement.classList.remove("active");
+
+            target.classList.add("active");
         }
+    }
+};
+
+(function () {
+    const sidebar = document.querySelector(".sidebar");
+
+    sidebar.addEventListener("click", _handleSimpleSidebar);
+})();
+
+// Remove simple sidebar functionality when proper sidebar functionality is loaded
+(function () {
+    document.addEventListener("DOMContentLoaded", (e) => {
+        const sidebar = document.querySelector(".sidebar");
+
+        sidebar.removeEventListener("click", _handleSimpleSidebar);
     });
 })();
 
