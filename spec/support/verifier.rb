c16df7c3a6930e163e84776069ea6751de57e3e4
Asbjørn Ulsberg <asbjorn@ulsberg.no>
2022-09-22 00:31:57 +0200

    DX-1846: Log with HTMLProofer's logger

--- spec/support/verifier.rb
+++ spec/support/verifier.rb
@@ -8,16 +8,19 @@ require 'concurrent'
 module SwedbankPay
   # Verifies the built HTML with HTMLProofer
   class Verifier
-    def initialize(auth_token)
+    def initialize(auth_token, log_level: :info)
+      @log_level = log_level
       @auth_token = auth_token
     end
 
     def verify(path)
       ensure_directory_not_empty!(path)
 
-      puts "Checking '#{path}' with HTMLProofer"
-
       proofer = HTMLProofer.check_directory(path, options)
+      @logger = proofer.logger
+
+      @logger.log(@log_level, "Checking '#{path}' with HTMLProofer")
+
       proofer.before_request { |request| before_request(request) }
       proofer.run
     end
@@ -28,13 +31,13 @@ module SwedbankPay
       uri = URI(request.base_url)
 
       unless uri.host.match('github\.(com|io)$')
-        puts "No authorization set for <#{uri}> as it's not matching github.com or github.io."
+        @logger.log(@log_level, "No authorization set for <#{uri}> as it's not matching github.com or github.io.")
         return
       end
 
       auth = "Bearer #{@auth_token}"
       request.options[:headers]['Authorization'] = auth
-      puts "Authorization set for <#{uri}>."
+      @logger.log(@log_level, "Authorization set for <#{uri}>.")
     end
 
     def ensure_directory_not_empty!(dir)
@@ -49,11 +52,12 @@ module SwedbankPay
         check_unrendered_link: true,
         checks: %w[Links Images Scripts UnrenderedLink],
         enforce_https: true,
-        log_level: :debug,
+        # log_level: :debug,
         only_4xx: true,
         parallel: { in_processes: Concurrent.processor_count },
-        typheous: { verbose: true },
+        # typheous: { verbose: false },
         url_ignore: [
+          'https://api.payex.com/',
           'http://www.wikipedia.org',
           'http://restcookbook.com/Basics/loggingin/'
         ]
