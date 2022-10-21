b7e99a0d7463e47b57b0a9c13026af6baae7f070
arebra <are.brandt-gundersen@payex.com>
2022-10-21 10:03:54 +0200

    DX-1846: Close but no cigar?

--- spec/support/verifier.rb
+++ spec/support/verifier.rb
@@ -66,6 +66,7 @@ module SwedbankPay
           'https://api.payex.com/',
           'http://www.wikipedia.org',
           'https://zend18.zendesk.com/hc/en-us/articles/219131697-HowTo-Implement-TLS-1-2-Support-with-the-cURL-PHP-Extension-in-Zend-Server'
+          'https://blogs.oracle.com/java/post/jdk-8-will-use-tls-12-as-default'
         ]
       }
     end
