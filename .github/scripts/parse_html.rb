8c7a4914234f9415c5c5675e6c10a9bc7fca26bb
oarset <oarset@gmail.com>
2023-08-14 17:09:49 +0200

    use rubyscript for parsing files

---
+++ .github/scripts/parse_html.rb
@@ -0,0 +1,18 @@
+# parse_html.rb
+require 'nokogiri'
+
+html_file_path = ARGV[0]
+
+doc = Nokogiri::HTML(File.open(html_file_path))
+
+title = doc.at_css('title').text
+content = doc.at_css('body').text
+
+# Add more metadata extraction as needed
+
+puts({
+  id: html_file_path,
+  url: "/#{html_file_path.sub('./_site/', '')}",
+  text: content.strip,
+  title: title.strip
+}.to_json)
